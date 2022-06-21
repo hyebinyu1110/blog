5d438ba077ffd96efad1ec6b6ffa7aa57793bbeb
hyebin yu <hyebinyu1110@gmail.com>
2022-06-21 16:26:34 +0900

    initial commit

---
+++ blog-backend/src/index.js
@@ -0,0 +1,13 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(ctx => {
+    console.log(ctx);
+    ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+    console.log('Listening to port 4000');
+})
+
