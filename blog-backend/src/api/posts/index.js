b142357ca71ac278bf7078c8bc0e096351309757
hyebinyu <hyebinyu1110@gmail.com>
2022-06-23 20:55:52 +0900

    initial commit

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -6,13 +6,8 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-
-const post = new Router(); // /api/posts/:id;
-
-post.get('/', postsCtrl.read);
-post.delete('/', postsCtrl.remove);
-post.patch('/', postsCtrl.update);
-
-posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+posts.get('/:id', postsCtrl.read);
+posts.delete('/:id', postsCtrl.remove);
+posts.patch('/:id', postsCtrl.update);
 
 export default posts;
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,18 +1,4 @@
 import Post from "../../models/post";
-import mongoose from "mongoose";
-import Joi from "joi";
-
-const { ObjectId } = mongoose.Types;
-
-export const checkObjectId = (ctx, next) => {
-    const { id } = ctx.params;
-    if (!ObjectId.isValid(id)) {
-        ctx.status = 400; // Bad Request
-        return;
-    }
-    return next();
-}
-
 
 /*
 POST /api/posts
@@ -24,23 +10,6 @@ POST /api/posts
 */
 
 export const write = async ctx => {
-    const schema = Joi.object().keys({
-        // 객체가 다음 필드를 가지고 있음을 검증
-        title: Joi.string().required(), // required()가 있으면 필수 항목
-        body: Joi.string().required(),
-        tags: Joi.array()
-            .items(Joi.string())
-            .required(), // 문자열로 이루어진 배열
-    });
-
-    // 검증하고 나서 검증 실패인 경우 에러 처리
-    const result = schema.validate(ctx.request.body);
-    if (result.error) {
-        ctx.status = 400; // Bad Request
-        ctx.body = result.error;
-        return;
-    }
-
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
@@ -55,106 +24,66 @@ export const write = async ctx => {
     }
 };
 
-/* GET /api/posts
+export const list = async ctx => { 
+    try{
+        const post = await Post.find().exec();
+        ctx.body = post;
+    }catch(e){
+        ctx.throw(500, e);
+    }
+};
+
+/*
+GET /api/posts/:id
 */
-
-export const list = async ctx => {
-    // query는 문자열이기때문에 숫자로 변환해 주어야 합니다.
-    // 값이 주어지지 않았다면 1을 기본으로 사용합니다.
-    const page = parseInt(ctx.query.page || '1', 10);
-
-    if (page < 1) {
-        ctx.status = 400;
+export const read = async ctx => { 
+const {id} = ctx.params;
+try{
+    const post = await Post.findById(id).exec();
+    if(!post){
+        ctx.status = 404; // Not found
         return;
     }
-    try {
-        const posts = await Post.find()
-            .sort({ _id: -1 })
-            .limit(10)
-            .skip((page - 1) * 10)
-            .lean() // 이 함수를 사용하면 처음부터 데이터를 JSON형태로 조회할수 있음
-            .exec();
-        const postCount = await Post.countDocuments().exec();
-        ctx.set('Last-Page', Math.ceil(postCount / 10)); // 응답헤더에 정보 추가
-        ctx.body = posts
-            // .map(post => post.toJSON())
-            .map(post => ({
-                ...post,
-                body:
-                    post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
-            }));
-    } catch (e) {
-        ctx.throw(500, e);
-    }
+    ctx.body = post;
+}catch(e){
+    ctx.throw(500, e);
+}
 };
 
 /*
-GET api/posts/:id
-*/
-export const read = async ctx => {
-    const { id } = ctx.params;
-    try {
-        const post = await Post.findById(id).exec();
-        if (!post) {
-            ctx.status = 404; // Not Found
-            return;
-        }
-        ctx.body = post;
-    } catch (e) {
-        ctx.throw(500, e);
-    }
-};
-
-/*
-DELETE /api/posts/:id
+DELETE api/posts/:id
 */
 export const remove = async ctx => {
     const { id } = ctx.params;
-    try {
+    try{
         await Post.findByIdAndRemove(id).exec();
-        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
-    } catch (e) {
+        ctx.status = 204;// No content( 성공하기는 했지만 응답할 데이터는 없음)
+    }catch(e){
         ctx.throw(500, e);
     }
-};
+ };
 
-/*
-PATCH /api/posts/:id
-{
-    title: '수정',
-    body: '수정 내용',
-    tags: ['수정', '태그]
-}
-*/
+ /*
+ PATCH /api/posts/:id
+ {
+     title: "수정",
+     body: "수정 내용",
+     tags: ["수정", "태그"]
+ }
+ */
 export const update = async ctx => {
-    const { id } = ctx.params;
-    // write에서 사용한 schema와 비슷한데, required()가 없습니다.
-    const schema = Joi.object().keys({
-        title: Joi.string(),
-        body: Joi.string(),
-        tags: Joi.array().items(Joi.string()),
-    });
-
-    const result = schema.validate(ctx.request.body);
-    if (result.error) {
-        ctx.status = 400; // Bad Request
-        ctx.body = result.error;
-        return;
-    }
-
-    try {
+    const { id }= ctx.params;
+    try{
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
-            // false일 때는 업데이트 되기전의 데이터를 반환합니다.
+            // false일때는 업데이트 되기전의 데이터를 반환합니다. 
         }).exec();
-        if (!post) {
+        if(!post){
             ctx.status = 404;
             return;
         }
         ctx.body = post;
-
-    } catch (e) {
+    }catch(e){
         ctx.throw(500, e);
     }
-
-};
+ };
